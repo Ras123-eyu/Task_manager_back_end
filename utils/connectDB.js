6ddab997552ebc781a37c5a88c5001f9344882ff
Ras123-eyu <eyoelsitotaw2020@gmail.com>
2025-12-10 13:50:06 +0300

    Update connectDB.js

--- utils/connectDB.js
+++ utils/connectDB.js
@@ -3,9 +3,13 @@ import mongoose from "mongoose";
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI({
-       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 30000,
+       serverSelectionTimeoutMS: 30000,    // Increase timeout
+  socketTimeoutMS: 45000,
+  bufferMaxEntries: 0,               // Disable mongoose buffering completely (recommended)
+  maxPoolSize: 10,                   // Prevent connection exhaustion
+  minPoolSize: 5,
+  maxIdleTimeMS: 30000,
+  family: 4           
     }));
 
     console.log("Database Connected");
